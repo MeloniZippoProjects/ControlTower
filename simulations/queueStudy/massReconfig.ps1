0256b6643f9fe263d0f0eca543d283637561f762
rzippo <raffaele.zippo@live.it>
2017-01-05 15:08:21 +0100

    Added massReconfig.ps1
    Comodo script ps per modificare contemporaneamente tutti gli ini in una
    cartella

---
+++ simulations/queueStudy/massReconfig.ps1
@@ -0,0 +1,34 @@
+﻿#ATTENTO ALLA DIRECTORY DOVE ESEGUI LO SCRIPT!!
+#Tieni d'occhio i file (sia target che non) per controllare che effetto c'è stato.
+#In caso di singolo errore, c'è il file di backup .old
+
+#Variabili di configurazione:
+
+$rootDir = "D:\Users\Raff\Documents\GitHub\PESC_Control_Tower\simulations\queueStudy\poissonian"
+$oldLinePattern = "interArrivalTime";
+$newLine = "SimNetwork.wg.interArrivalTime = exponential(100min)";
+
+#Inizio script
+
+$currentDir = pwd
+cd $rootDir
+
+$inis = Get-ChildItem -Recurse -Filter "queueStudy.ini"
+
+foreach($ini in $inis)
+{
+    #Backup
+    Copy-Item -Path $ini.FullName -Destination ($ini.FullName + ".old") 
+    
+    #Ricerca e sostituzione
+    $content = type $ini.FullName;
+    $matches = Select-String -Path $ini.FullName -Pattern $oldLinePattern;
+    foreach($match in $matches)
+    {
+        $content[$match.LineNumber - 1] = $newLine;
+    }
+
+    Set-Content -Path $ini.FullName -Value $content;
+}
+
+cd $currentDir
