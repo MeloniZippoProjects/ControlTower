efb5a3491923948243c48cc69f949dc13dad10d9
rzippo <raff@UbuntuWorkVM>
2017-01-05 13:10:54 +0100

    Working powershell script, simulation results for the deterministic case

--- simulations/queueStudy/deterministic/QueueStudy.ps1
+++ simulations/queueStudy/deterministic/QueueStudy.ps1
@@ -1,3 +1,4 @@
+$root = pwd
 $inis = Get-ChildItem -Recurse -Filter "queueStudy.ini"
 
 $runSimulation =
@@ -6,7 +7,7 @@ $runSimulation =
 
     cd $ini.DirectoryName
     
-    for ($i = 0, $i -lt 70, $i++)
+    for ($i = 0; $i -lt 70; $i++)
     {
         ../../../../src/PESC_Control_Tower -r $i -u Cmdenv -c QueueMeasurement -n ../../..:../../../../src --debug-on-errors=false queueStudy.ini
     }
@@ -14,10 +15,12 @@ $runSimulation =
 
 foreach( $ini in $inis )
 {
-    Start-Job -ScriptBlock $runSimulation -ArgumentList $ini   
+    Invoke-Command -ScriptBlock $runSimulation -ArgumentList $ini   
 }
 
+cd $root
 
+#Get-Job | Wait-Job
 
 #$ cd /home/raff/omnetpp-5.0/controltower/PESC_Control_Tower/simulations/queueStudy/deterministic/rho0.1
 #$ ../../../../src/PESC_Control_Tower -r 69 -u Cmdenv -c QueueMeasurement -n ../../..:../../../../src --debug-on-errors=false queueStudy.ini
