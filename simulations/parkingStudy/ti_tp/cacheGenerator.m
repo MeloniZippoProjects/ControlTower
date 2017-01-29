30507a56442607108e6fbbde2f332d1267269f7e
rzippo <raffaele.zippo@live.it>
2017-01-29 19:34:42 +0100

    Cleanup for cacheGenerators

--- simulations/parkingStudy/ti_tp/cacheGenerator.m
+++ simulations/parkingStudy/ti_tp/cacheGenerator.m
@@ -1,29 +1,15 @@
 distributions = [ string('exponential'); string('lognormal') ];
 scenarios = [string('i15p60'); string('i15p30'); string('i30p60'); string('i30p30');];
 
-timeVectors = [ string('takeoffQueue_queueTime') ; string('landingQueue_queueTime'); string('wg_responseTime') ];
-lengthVectors = [ string('takeoffQueue_queueLength') ; string('landingQueue_queueLength'); string('parkingLot_parkingOccupancy') ];
+vector = 'parkingLot_parkingOccupancy';
 
 for distIdx = 1 : size(distributions, 1)
     dist = distributions(distIdx).char; 
     cd(dist);
 
-    for vectorIdx = 1 : size(lengthVectors, 1)
-        vector = lengthVectors(vectorIdx).char;
-
-        parfor scenIdx = 1 : length(scenarios)
-            scenario = scenarios(scenIdx, :);
-            samples = loadLengthSamples(vector, scenario);
-        end
-    end
-
-    for vectorIdx = 1 : size(timeVectors, 1)
-        vector = timeVectors(vectorIdx).char;
-
-        parfor scenIdx = 1 : length(scenarios)
-            scenario = scenarios(scenIdx, :);
-            samples = loadTimeSamples(vector, scenario);
-        end
+    parfor scenIdx = 1 : length(scenarios)
+        scenario = scenarios(scenIdx, :);
+        samples = loadLengthSamples(vector, scenario);
     end
 
     cd ..
